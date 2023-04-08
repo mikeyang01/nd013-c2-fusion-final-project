0bc6799a7e20fe7a84ba791c9fa527776a6d7662
Mike <ytianhui2004@gmail.com>
2023-04-08 19:04:16 +0800

    Update trackmanagement.py
        sigma_p66 = params.sigma_p66
        P_vel = np.matrix([[sigma_p44**2, 0, 0],[0, sigma_p55**2, 0],[0,0, sigma_p55**2]])
    The second sigma_p55 should be replaced with sigma_p66.

--- student/trackmanagement.py
+++ student/trackmanagement.py
@@ -68,7 +68,7 @@ class Track:
         sigma_p44 = params.sigma_p44
         sigma_p55 = params.sigma_p55
         sigma_p66 = params.sigma_p66
-        P_vel = np.matrix([[sigma_p44**2, 0, 0],[0, sigma_p55**2, 0],[0,0, sigma_p55**2]])
+        P_vel = np.matrix([[sigma_p44**2, 0, 0],[0, sigma_p66**2, 0],[0,0, sigma_p55**2]])
 
         # overall covariance initialization
         self.P = np.zeros((6,6))
